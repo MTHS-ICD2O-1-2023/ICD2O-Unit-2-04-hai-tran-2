34731e144e5af4b4c45f0618c49c7723e548585c
Hai Tran <159185497+hai-tran-2@users.noreply.github.com>
2024-03-21 18:32:25 +0000

    added first webpage

--- js/script.js
+++ js/script.js
@@ -1,5 +1,22 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Hai Tran
+// Created on: March 2024
 // This file contains the JS functions for index.html
+
+"use strict"
+
+/**
+ * This function calculates area of a triangle.
+ */
+function calculateAreaOfTriangle () {
+  // input
+  const heightOfTriangle = parseInt(document.getElementById('height-of-triangle').value)
+  const baseOfTriangle = parseInt(document.getElementById('base-of-triangle').value)
+
+  // process
+  const areaOfTriangle = (heightOfTriangle * baseOfTriangle) /2
+
+  // output
+  document.getElementById('area').innerHTML = 'The area is: ' + areaOfTriangle + ' cm²'
+}
